5be7f81df2e1edae9ab6cac345b554a85a29d018
eyup agaogullari <eeyuptr@gmail.com>
2023-01-20 04:32:54 +0300

    odev tamamlandi

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+  Console.WriteLine("Hello, \n World! \n isminiz nedir?");
+        string isim = Console.ReadLine();
+        Console.WriteLine("Şimdi de doğum yılınızı giriniz");
+        int yil = Convert.ToInt32(Console.ReadLine());
+        int yas = DateTime.Now.Year - yil;
+        Console.WriteLine($"{isim}'in yasi {yas}");
